858882fb92de80c1ac8c18140fa1f64368845e87
Laio Zago <laiozago@gmail.com>
2024-01-14 23:53:19 -0300

    pontuação feita

--- EXERCISE_6.py
+++ EXERCISE_6.py
@@ -1,22 +1,26 @@
 def pig_latin(word):
     is_upper = word[0].isupper()  # Verifica se a primeira letra é maiúscula
     word = word.lower()  # Converte a palavra para minúsculas para aplicar as regras
-    if word[0] in 'aeiou':
+
+    # Separa a pontuação da palavra
+    punc = ''
+    while word and word[-1].isalpha() is False:
+        punc = word[-1] + punc
+        word = word[:-1]
+
+    if word and word[0] in 'aeiou':
         new_word = word + 'way'
     else:
-        # Separa a pontuação da palavra
-        punc = ''
-        while word[-1].isalpha() is False:
-            punc = word[-1] + punc
-            word = word[:-1]
-        new_word = word[1:] + word[0] + 'ay' + punc
+        new_word = word[1:] + word[0] + 'ay'
+
+    # Adiciona a pontuação de volta à palavra
+    new_word += punc
 
     # Mantém a capitalização original
     if is_upper:
         new_word = new_word.capitalize()
 
     return new_word
-
 
 def pig_latin_sentence():
     frase = input('Entre a frase que deseja traduzir: ')
@@ -25,7 +29,6 @@ def pig_latin_sentence():
     for palavra in palavras:
         novas_palavras.append(pig_latin(palavra))
     print(' '.join(novas_palavras))
-
 
 # Chamada da função principal
 pig_latin_sentence()
