532201b30df376f30fbfdf685b02a8f3161bc0fe
Laio Zago <laiozago@gmail.com>
2024-01-19 22:30:42 -0300

    2.

--- exercicios/CHPT-03-Lists-and-tuples/Exer-13-Printing-tuple-records/BTE_2.py
+++ exercicios/CHPT-03-Lists-and-tuples/Exer-13-Printing-tuple-records/BTE_2.py
@@ -5,3 +5,10 @@ Oscar awards last year. Ask the user whether they want to sort the list
 by title, length, or director’s name, and then present the list sorted
 by the user’s choice of axis.
 '''
+lista = [
+    ('The Shawshank Redemption', 142, 'Frank Darabont'),
+    ('The Godfather', 175, 'Francis Ford Coppola'),
+    ('The Godfather: Part II', 202, 'Francis Ford Coppola'),
+    ('The Dark Knight', 152, 'Christopher Nolan'),
+    ('12 Angry Men', 96, 'Sidney Lumet'),
+]
